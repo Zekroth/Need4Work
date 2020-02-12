c899dd38ffdc3050d2f22054fef7cceb10d2b0d9
gabclement <gabclement@GABCLEMENTW0.italy.itroot.adnet>
2020-02-12 18:00:10 +0100

    Modifica classe inserzionista

--- Need4Work/src/com/need4work/models/Inserzionista.java
+++ Need4Work/src/com/need4work/models/Inserzionista.java
@@ -1,5 +1,25 @@
 package com.need4work.models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Inserzionista extends User {
+	
+	ArrayList<Job> listaInserzione = new ArrayList<>();
+	
+	//FIELDS
+	
+	//CONSTRUCTORS
+	
+	//METHODS
+	//Creazione di una nuova inserzione
+	public void CreaInserzione(String titolo, String descrizione, Date data, float importoMax) {
+		//Job lavoro=new Job(titolo, descrizione, data, importoMax);
+		//listaInserzione.add(lavoro);
+	}
+	
+	public void ScriviRecensione(String titolo, String recensione, int valore) {
+		
+	}
 
 }
