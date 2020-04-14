10b41ee9a7dcd9f70ccaa207211ed26b0ec73aff
Zekroth <edoardo.barchiesi@hotmail.it>
2020-04-14 16:19:34 +0200

    Riordine directory e aggiunta LoginSignupController

---
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/controllers/LoginSignupController.java
@@ -0,0 +1,65 @@
+package it.itsrizzoli.Spring.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginSignupController {
+	
+	@GetMapping("/signUpInserzionista")
+	public String signUpInserzionista(SignUpInserzionistaForm signUpInserzionistaForm) {
+		return "signUpInserzionista";
+	}
+	
+	@PostMapping("/signUpInserzionista")
+	public String postSignUpInserzionista(@Valid SignUpInserzionistaForm signUpInserzionistaForm, BindingResult res) {
+		if (res.hasErrors())
+			return "signUpInserzionista";
+		
+		
+		return "iscrizioneEseguita";
+	}
+	
+	@GetMapping("/signUpProfessionista")
+	public String signUpProfessionista(SignUpProfessionistaForm signUpProfessionistaForm) {
+		return "signUpProfessionista";
+	}
+	
+	@PostMapping("/signUpProfessionista")
+	public String postSignUpProfessionista(@Valid SignUpProfessionistaForm signUpProfessionistaForm, BindingResult res) {
+		if (res.hasErrors())
+			return "signUpProfessionista";
+		
+		
+		return "iscrizioneEseguita";
+	}
+	
+	@GetMapping("/login")
+	public String login(LoginForm loginForm) {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(@Valid LoginForm loginForm, BindingResult res) {
+		if (res.hasErrors())
+			return "login";
+		
+		
+		return null;	//da modificare per far andare in una pagina o l'altra a seconda di chi si sia autenticato
+	}
+	
+	@GetMapping("/sceltaAccount")
+	public String sceltaAccount() {
+		return "sceltaAccount";
+	}
+	
+	@GetMapping("/iscrizioneEseguita")
+	public String iscrizioneEseguita() {
+		return "iscrizioneEseguita";
+	}
+	
+}
--- Need4Work_Spring/src/main/java/it/itsrizzoli/Spring/models/configs/package-info.java
+++
@@ -1,1 +0,0 @@
-package it.itsrizzoli.Spring.models.configs;
