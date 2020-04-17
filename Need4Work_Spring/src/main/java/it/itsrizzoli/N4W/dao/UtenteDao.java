93c76e205519c5280283ff96aa668e5967fb42b0
Gabriele Clementi <60978981+Gabry26@users.noreply.github.com>
2020-04-17 11:44:21 +0200

    creato dao per classe utente

---
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/UtenteDao.java
@@ -0,0 +1,18 @@
+package it.itsrizzoli.N4W.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.itsrizzoli.N4W.models.db.Utente;
+
+public interface UtenteDao extends CrudRepository<Utente, Long>{
+	
+	List<Utente> findByNome(String nome);
+	List<Utente> findByCognome(String cognome);
+	List<Utente> findByPassword(String password);
+	List<Utente> findByPaese(String paese);
+	List<Utente> findByProvincia(String provincia);
+	Utente findByEmail(String email);
+
+}
