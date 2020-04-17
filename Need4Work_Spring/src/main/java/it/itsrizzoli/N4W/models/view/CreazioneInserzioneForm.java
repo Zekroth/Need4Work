a2e67efa75ae3cda87a37f2d4f8081792d1eb049
Gabriele Clementi <60978981+Gabry26@users.noreply.github.com>
2020-04-17 14:19:15 +0200

    creata form per le inserzioni/aste

---
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/models/view/CreazioneInserzioneForm.java
@@ -0,0 +1,66 @@
+package it.itsrizzoli.N4W.models.view;
+
+import java.sql.Date;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreazioneInserzioneForm {
+	
+	@Size(min=5,max=100)
+	String titolo;
+	
+	@Size(min=30,max=500)
+	String commento;
+	
+	@DecimalMax(value = "9999.99")
+	double prezzoPartenza;
+	
+	@NotNull
+	String professioneRichiesta;
+	
+	@NotNull
+	Date dataFine;
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public String getCommento() {
+		return commento;
+	}
+
+	public void setCommento(String commento) {
+		this.commento = commento;
+	}
+
+	public double getPrezzoPartenza() {
+		return prezzoPartenza;
+	}
+
+	public void setPrezzoPartenza(double prezzoPartenza) {
+		this.prezzoPartenza = prezzoPartenza;
+	}
+
+	public String getProfessioneRichiesta() {
+		return professioneRichiesta;
+	}
+
+	public void setProfessioneRichiesta(String professioneRichiesta) {
+		this.professioneRichiesta = professioneRichiesta;
+	}
+
+	public Date getDataFine() {
+		return dataFine;
+	}
+
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+
+}
