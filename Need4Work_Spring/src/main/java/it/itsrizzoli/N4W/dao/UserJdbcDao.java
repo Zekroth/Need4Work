a6eed6d4b58573e6b3c7f4d3228ba68e8c5ba81e
GabrieleClementi <g.clementi@itsrizzoli.it>
2020-04-20 09:36:02 +0200

    creata classe jdbcDao per il login

---
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/UserJdbcDao.java
@@ -0,0 +1,37 @@
+package it.itsrizzoli.N4W.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import it.itsrizzoli.N4W.models.db.Utente;
+
+@Repository
+public class UserJdbcDao {
+	
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	public List<Utente> login(String email, String password) {
+        return jdbcTemplate.query(
+                "select * from utenti where email = ? and password = ?",
+                new Object[]{email, password},
+                (rs, rowNum) ->
+                        new Utente(
+                        		rs.getString("nome"),
+                        		rs.getString("cognome"),
+                        		rs.getDate("dataNascita"),
+                        		rs.getString("via"),
+                        		rs.getString("paese"),
+                        		rs.getString("provincia"),
+                        		rs.getString("email"),
+                        		rs.getString("cellulare"),
+                        		rs.getString("password"),
+                        		rs.getInt("cap")
+                        )
+        );
+    }
+
+}
