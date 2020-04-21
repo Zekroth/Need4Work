dfcbd9d9ba9fbe23420b3605d1f46e346f798f9b
Zekroth <edoardo.barchiesi@hotmail.it>
2020-04-21 17:10:33 +0200

    navbar

--- Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/controllers/ProfessionistaController.java
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/controllers/ProfessionistaController.java
@@ -58,4 +58,6 @@ public class ProfessionistaController {
 		return "profiloProfessionista";
 	}
 	
+	
+	
 }
--- Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/ProfessioneDao.java
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/ProfessioneDao.java
@@ -14,6 +14,6 @@ public interface ProfessioneDao extends CrudRepository<Professione, Long>{
 	Professione findById(long id);
 	List<Professione> findByTipologia(String tipologia);
 	
-	@Query(value=" select p from Professione p inner join Professionista pro where pro.professionistaId.utente.email = ?1")
+	@Query(value=" select p from Professione p inner join Professionista pro where pro.professionistaId.utente.email = ?1 and p.id = pro.professionistaId.professione.id")
 	List<Professione> findByUser(@Param("email") String email);
 }
