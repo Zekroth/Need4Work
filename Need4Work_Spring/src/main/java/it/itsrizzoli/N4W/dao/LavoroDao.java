27de43a65b9febcbc0215b3b6a4808564beffbd2
Zekroth <edoardo.barchiesi@hotmail.it>
2020-04-22 11:38:48 +0200

    fix profession display

--- Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/LavoroDao.java
+++ Need4Work_Spring/src/main/java/it/itsrizzoli/N4W/dao/LavoroDao.java
@@ -7,38 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import it.itsrizzoli.N4W.models.db.Lavoro;
 
-public class LavoroDao implements DAO<Lavoro>{
+public interface LavoroDao extends CrudRepository<Lavoro, Long>{
 	
 	
-
-	@Override
-	public Optional<Lavoro> get(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Lavoro> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void save(Lavoro d) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void update(Lavoro d, String[] params) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void delete(Lavoro d) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
