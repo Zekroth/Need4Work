ae6fe9cbec213de67b2edf220422a45e91f1fba5
Zekroth <edoardo.barchiesi@hotmail.it>
2020-04-24 10:46:56 +0200

    fix db

--- n4w.sql
+++ n4w.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Creato il: Apr 23, 2020 alle 17:02
+-- Creato il: Apr 24, 2020 alle 08:45
 -- Versione del server: 8.0.18
 -- Versione PHP: 7.3.12
 
@@ -44,7 +44,16 @@ CREATE TABLE IF NOT EXISTS `asta` (
   KEY `email_utente` (`email_utente`),
   KEY `email_vincitore` (`email_vincitore`),
   KEY `professione_richiesta` (`professione_richiesta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `asta`
+--
+
+INSERT INTO `asta` (`id_asta`, `email_utente`, `data_inizio`, `data_fine`, `email_vincitore`, `prezzo_partenza`, `prezzo_chiusura`, `titolo`, `commento`, `professione_richiesta`) VALUES
+(1, 'edoardo.barchiesi@hotmail.it', '2020-04-24 00:00:00', '2020-04-24 00:00:00', NULL, '500.15', NULL, 'Creazione sito web', 'Ho bisogno di un programmatore che mi crei un sito web', 3),
+(2, 'edoardo.barchiesi@hotmail.it', '2020-04-24 00:00:00', '2020-04-24 00:00:00', NULL, '500.16', NULL, 'Creazione sito web', 'Ho bisogno di un programmatore che mi crei un sito web', 3),
+(3, 'edoardo.barchiesi@hotmail.it', '2020-04-24 00:00:00', '2020-04-25 00:00:00', NULL, '500.16', NULL, 'Creazione sito web', 'Ho bisogno di un programmatore che mi crei un sito web', 3);
 
 -- --------------------------------------------------------
 
@@ -90,7 +99,17 @@ CREATE TABLE IF NOT EXISTS `professione` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipologia` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `professione`
+--
+
+INSERT INTO `professione` (`id`, `tipologia`) VALUES
+(1, 'Elettricista'),
+(2, 'Idraulico'),
+(3, 'Scimmia Programmatrice'),
+(4, 'Gabriel');
 
 -- --------------------------------------------------------
 
@@ -103,9 +122,16 @@ CREATE TABLE IF NOT EXISTS `professionista` (
   `email_utente` varchar(300) NOT NULL,
   `id_professione` int(11) NOT NULL,
   PRIMARY KEY (`email_utente`,`id_professione`),
-  UNIQUE KEY `email_utente` (`email_utente`),
   UNIQUE KEY `id_professione` (`id_professione`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `professionista`
+--
+
+INSERT INTO `professionista` (`email_utente`, `id_professione`) VALUES
+('edoardo.barchiesi@hotmail.it', 2),
+('edoardo.barchiesi@hotmail.it', 3);
 
 -- --------------------------------------------------------
 
@@ -141,6 +167,13 @@ CREATE TABLE IF NOT EXISTS `utente` (
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `utente`
+--
+
+INSERT INTO `utente` (`nome`, `cognome`, `data_nascita`, `via`, `cap`, `paese`, `provincia`, `email`, `cellulare`, `password`) VALUES
+('Edoardo', 'Barchiesi', '1998-12-14', 'via franchi maggi 4, Rozzano', 20089, 'Italia', 'MI', 'edoardo.barchiesi@hotmail.it', '3474295393', 'Edogaming98');
 
 --
 -- Limiti per le tabelle scaricate
